2051b5eb43f49c06601bfd9f1e498648fd37ce36
Philip Yuen <philipyuench@gmail.com>
2012-05-12 11:45:13 +0800

    fix menu not hightlight at show

--- app/javascripts/footer.js
+++ app/javascripts/footer.js
@@ -1,14 +1,27 @@
 (function(swim) {
   swim.footer = {
+    page: "",
     initiate: function() {
-      $(document).live("pageshow", function(){
+      $(document).live( "pageinit", _.bind(function(){
+        $(".ui-footer .ui-navbar li a").die("click").live("click", _.bind(function(event) {
+          _.each(["schedule", "records", "information", "about", "register"], _.bind(function(p) {
+            if ($(event.target).closest("a").hasClass("footer-" + p))  {
+              this.page = p;
+              return true;
+            }
+          }, this))
+        }, this));
+      }, this));
+
+      $(document).live("pageshow", _.bind(function(){
         var size = window.innerWidth / 5;
         $(".ui-btn-icon-top .ui-btn-inner").css("padding", size + "px 0 0");
         $(".ui-footer .ui-navbar .ui-btn .ui-icon").css({
           height: size,
           width: size
         });
-      });
+        $(".ui-footer .ui-navbar li a.footer-" + this.page + ":visible").addClass("ui-btn-active");
+      }, this));
     }
   };
 })(swim);
