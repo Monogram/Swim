00af4a38802ab133e8ccc3be70a1cc6123a400ef
Philip Yuen <philipyuench@gmail.com>
2012-05-15 01:13:01 +0800

    fix ios email encoding

--- app/javascripts/schedule.js
+++ app/javascripts/schedule.js
@@ -18,24 +18,24 @@
         $("#schedule .submit").die("click").live("click", _.bind(function(event) {
           event.preventDefault();
           var mailto = "mailto:a.tough.swimming.club@gmail.com";
-          mailto += "?subject=" + this.strToBig5("學員評估");
-          mailto += "&body=" + this.strToBig5("姓名: " + $("#schedule #name").val().trim()) + "%0D%0A";
-          mailto += this.strToBig5("電話: " + $("#schedule #phone").val().trim()) + "%0D%0A";
-          mailto += this.strToBig5("年齡: " + $("#schedule #age").val()) + "%0D%0A";
-          mailto += "%0D%0A";
-          mailto += this.strToBig5("已選項目:") + "%0D%0A";
+          mailto += "?subject=" + encodeURI("學員評估");
+          mailto += "&body=" + encodeURI("姓名: " + $("#schedule #name").val().trim()) + "%0A";
+          mailto += encodeURI("電話: " + $("#schedule #phone").val().trim()) + "%0A";
+          mailto += encodeURI("年齡: " + $("#schedule #age").val()) + "%0A";
+          mailto += "%0A";
+          mailto += encodeURI("已選項目:") + "%0A";
           var schedule = swim.storage.get("schedule") || {};
           for (var i = 1; i <= 7; ++i) {
-            mailto += "%0D%0A";
-            mailto += this.strToBig5($(".schedule-" + i + " .ui-link-inherit").text().trim()) + "%0D%0A";
+            mailto += "%0A";
+            mailto += encodeURI($(".schedule-" + i + " .ui-link-inherit").text().trim()) + "%0A";
             var index = 0;
             for (var j = 1; j <= 6; ++j) {
               if (schedule[i + "-" + j]) {
-                mailto += (++index) + ".%20" + this.strToBig5($("label[for=schedule-" + i + "-" + j + "] .ui-btn-text").text().trim()) + "%0D%0A";
+                mailto += (++index) + ".%20" + encodeURI($("label[for=schedule-" + i + "-" + j + "] .ui-btn-text").text().trim()) + "%0A";
               }
 					  }
             if (index === 0) {
-              mailto += "--%0D%0A";
+              mailto += "--%0A";
             }
           }
           window.location.href = mailto;
