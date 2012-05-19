b33812e6eb0ff8d025960996d9c63c93db109f7e
Philip Yuen <philipyuench@gmail.com>
2012-05-20 00:50:55 +0800

    fix poster size

--- app/javascripts/poster.js
+++ app/javascripts/poster.js
@@ -1,16 +1,29 @@
 (function(swim) {
   swim.poster = {
     initiate: function() {
-      $(document).bind("pageinit", function(){
-        var img_height = 2048;
+      $(document).bind("pageinit", _.bind(function(){
+        _.defer(this.resizeContent);
+      }, this));
+      delete this.initiate;
+    },
+    resizeContent: function() {
         var img_width = 1536;
-        var height = window.innerHeight - $(".ui-header:visible").outerHeight(true) - $(".ui-footer:visible").outerHeight(true);
+        var img_height = 1652;
+        var height = window.innerHeight -
+          $(".ui-header:visible").outerHeight(true) -
+          $(".ui-footer:visible").outerHeight(true) -
+          $(".ui-content:visible").outerHeight(true) +
+          $(".ui-content:visible").height();
+        height = Math.min(height, img_height);
+        var width = img_width / img_height * height;
+        if (width > window.innerWidth) {
+          width = window.innerWidth;
+          height = img_height / img_width * width;
+        }
         $("#poster .poster").css({
-          "background-size":  (img_width / img_height * height) + " " + height,
+          "background-size":  width + " " + height,
           "height": height
         });
-      });
-      delete this.initiate;
     }
   };
 })(swim);
